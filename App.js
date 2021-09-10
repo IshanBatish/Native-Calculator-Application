26a5260f17fe7d2bf435a5c81aeab47f1b9b9d84
Ishan Batish <71932861+IshanBatish@users.noreply.github.com>
2021-09-10 16:00:55 -0400

    Button Learned

--- App.js
+++ App.js
@@ -1,23 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Shabash Ishan, Finally You did it. Really Nice and happy to see this</Text>
+    <View style={{padding: 30}}>
+      <View style={{flexDirection: 'row',justifyContent:'space-between'}}>
+        <TextInput placeholder="Name" style={{borderBottomColor: 'black', borderBottomWidth:1, padding:10}}/>
+        <Button title="ADD" />
+        <View>
+
+        </View>
+      </View>
+      <Text>Finally You did it. Really Nice and happy to see this</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
+/*
 const styles = StyleSheet.create({
   container: {
     flex: 14,
-    padding:  2,
     color: '#ff0000',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+*/
