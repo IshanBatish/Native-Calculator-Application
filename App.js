a6841c0a85220c93cf603f9b2b38d6ab97953ae2
Ishan Batish <71932861+IshanBatish@users.noreply.github.com>
2021-09-10 15:30:32 -0400

    Android Emulator Configured

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Shabash Ishan, Finally You did it. Really Nice and happy to see this</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 14,
+    padding:  2,
+    color: '#ff0000',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
