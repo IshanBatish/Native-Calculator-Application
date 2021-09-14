a9c80ab5fec2ca626bedcf39ab724af67590b9be
Ishan Batish <71932861+IshanBatish@users.noreply.github.com>
2021-09-14 17:43:04 -0400

    Completed Build

--- App/util/calculator.js
+++ App/util/calculator.js
@@ -1,9 +1,11 @@
+//Defining InitialState
 export const initialState = {
     currentValue: "0",
     operator: null,
     previousValue: null,
 }; 
 
+//Handler 
 export const handleNumber = (value, state)=> {
     if (state.currentValue === "0"){
         return {currentValue: `${value}` };
@@ -15,35 +17,49 @@ export const handleNumber = (value, state)=> {
 };
 
 export const handleEqual = state => {
+
+    //All Defined Operators and Value are stored as a State
     const { currentValue,previousValue,operator} = state;
 
+    //Defining Second Input 
     const current = parseFloat(currentValue);
+    
+    //Defining First Input
     const previous = parseFloat(previousValue);
+    
+    
+    //Defining Reset 
     const resetState = {
         operator: null,
         previousValue: null,
     };
 
+
+    //Defining Divide Operator
     if (operator == "/"){
         return {
             currentValue: previous / current,
+            //Plus the Initial State of the Calculator for Reset
             ...resetState
         };
     }
 
+    //Defining Multiplication Operator
     if (operator === "*"){
         return{
             currentValue: previous * current,
             ...resetState
         };
     }
+    //Defining Addition Operator
     if (operator === "+"){
         return{
             currentValue: previous + current,
             ...resetState
             };
-            
     }
+
+    //Defining Subtraction Operator
     if (operator === "-"){
         return{
             currentValue: previous + current,
@@ -51,26 +67,38 @@ export const handleEqual = state => {
         };
     }
 
+    //Setting State
     return state;
 };
+
+
+//
 const calculator =(type,value,state) => {
     switch(type){
+        //User Pressing any Number Key
         case "number":
             return handleNumber(value,state);
+        
+        //User Pressing any operator Key
         case "operator":
             return {
                 operator: value,
                 previousValue: state.currentValue,
                 currentValue: "0"
             };
+
+        //User pressing equal key
         case "equal": 
             return handleEqual(state);
+        //User Pressing Clear Key
         case "clear":
             return initialState;
+        //User Pressing Positive Negative Integers
         case "posneg":
             return {
                 currentValue: `${parseFloat(state.currentValue) * -1}`
               };
+        //User Pressing Percentage Key
         case "percentage":
             return {
                 currentValue: `${parseFloat(state.currentValue) * 0.01}`
